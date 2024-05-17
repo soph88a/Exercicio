c0083f734bfef3c3010fe4d3273acc396878f0d9
SOPHIA DA SILVA MORAL <00001132684262sp@aluno.educacao.sp.gov.br>
2024-05-17 17:28:48 -0300

    firt commit

---
+++ exercicio 1.py
@@ -0,0 +1,8 @@
+#solicitar ao usuario que digite um numero
+numero = float (input ("Digite um numero:"))
+if numero > 0:
+    print ("o numero é positivo")
+elif numero < 0:
+    print("o numero é negativo")
+else:
+    print("o numero é zero")
